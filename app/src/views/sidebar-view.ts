de390c411c23bd08f0fbcbbbec12154e60e4b109
Rodrigo Heinzen <rodrigo.h.m.112200@gmail.com>
2023-03-02 17:19:16 -0300

    responsive

--- app/src/views/sidebar-view.ts
+++ app/src/views/sidebar-view.ts
@@ -6,9 +6,9 @@ export class SidebarView extends View<ISidebarItem[]> {
     protected template(model: ISidebarItem[]): string {
         return `
         
-        <ul class="p-3">
+        <ul class="p-0 p-sm-3">
             ${ model.map(item => `
-            <li class="p-3">
+            <li class="p-sm-3 align-middle">
                 <a href="#" id="${ item.id }" aria-label="Acessar tela ${ item.description }">
                     <i class="${ item.icon }"></i>
                     <span class="compact-invisible ms-3">${ item.description }</span>
