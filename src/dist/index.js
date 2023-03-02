6c4f018c969788db6b1e1e3db514e6a794b31b16
Rodrigo Heinzen <rodrigo.h.m.112200@gmail.com>
2023-03-02 17:39:17 -0300

    removing dist

--- src/dist/index.js
+++
@@ -1,7 +0,0 @@
-import { EmployeesListController } from "./controllers/employees-list-controller.js";
-import { SidebarController } from "./controllers/sidebar-controller.js";
-const navigationHandler = new SidebarController();
-let currentPage = new EmployeesListController();
-navigationHandler.onPageChange(controller => {
-    currentPage = controller;
-});
--- src/dist/shared/decorators/dom-injection-decorator.js
+++
@@ -1,8 +0,0 @@
-export function DomInjector(selector) {
-    return function (target, propKey) {
-        const get = function () {
-            return document.querySelector(selector);
-        };
-        Object.defineProperty(target, propKey, { get });
-    };
-}
